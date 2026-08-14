f0ce9fb1bfbd1b8fda432737b902cf530f25cede
gtut_Betty <gtut3252300@gmail.com>
2026-08-14 17:57:03 +0800

    fix:變更路徑

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: env.VITE_WEB_PATH,
+    base: './',
     define: { "process.env": {} },
     css: {
       preprocessorOptions: {
